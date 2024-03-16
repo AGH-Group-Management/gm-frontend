943401a7f189280e50832e27b2e7bd19a200afab
Krzysztof Solecki <krzyss2019@gmail.com>
2024-03-16 08:57:11 +0100

    Created login form component with basic validation and installed eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, LoginComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'gm-frontend';

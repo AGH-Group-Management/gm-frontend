8e47a3cf9148f02ea245e4ec837199505efa7ecf
Krzysztof Solecki <krzyss2019@gmail.com>
2024-03-18 18:25:58 +0100

    Created Dean's requests list with sorting and filtering temporarly based on mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { DeanRequestsListComponent } from "./dean-requests-list/dean-requests-list.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, LoginComponent, NavbarComponent]
+    imports: [RouterOutlet, LoginComponent, NavbarComponent, DeanRequestsListComponent]
 })
 export class AppComponent {
   title = 'gm-frontend';

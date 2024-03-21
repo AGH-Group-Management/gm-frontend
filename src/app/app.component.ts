ac9eaa2fb5999f6cfd868a97e7667d60e264f296
Marcin <mrcnsaja@gmail.com>
2024-03-21 22:56:31 +0100

    Added components for assigned courses view with mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { CoursesAssignedComponent } from './courses-assigned/courses-assigned.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, LoginComponent, NavbarComponent]
+    imports: [RouterOutlet, LoginComponent, NavbarComponent, CoursesAssignedComponent]
 })
 export class AppComponent {
   title = 'gm-frontend';
---
+++ src/app/courses-assigned/courses-assigned.component.ts
@@ -0,0 +1,47 @@
+import { Component, ViewChild } from '@angular/core';
+import {
+  MatSelect,
+  MatFormField,
+  MatLabel,
+  MatOption,
+} from '@angular/material/select';
+import { CommonModule } from '@angular/common';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { CourseStudentTableComponent } from '../course-student-table/course-student-table.component';
+import { Course } from './courses-assigned.model';
+import { semestersData, semester1Data, semester2Data } from './mock-data';
+
+@Component({
+  selector: 'app-courses-assigned',
+  standalone: true,
+  imports: [
+    MatSelect,
+    MatFormField,
+    MatLabel,
+    MatOption,
+    CommonModule,
+    MatAccordion,
+    MatExpansionModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    CourseStudentTableComponent,
+  ],
+  templateUrl: './courses-assigned.component.html',
+  styleUrl: './courses-assigned.component.scss',
+})
+export class CoursesAssignedComponent {
+  courses: Course[] = [];
+  selectedSemester = 0;
+  semesters = semestersData;
+
+  @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+  selectSemester(event: any) {
+    this.selectedSemester = event.value;
+    this.courses = this.selectedSemester === 1 ? semester1Data : semester2Data;
+  }
+}

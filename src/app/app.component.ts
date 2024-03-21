331f4e5186f2fb788b57eb142cacd7e1b1891e14
Marcin <mrcnsaja@gmail.com>
2024-03-21 23:25:35 +0100

    Added home component, page not found component and routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CoursesAssignedComponent } from './courses-assigned/courses-assigned.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, LoginComponent, NavbarComponent, CoursesAssignedComponent]
+    imports: [RouterOutlet, RouterLink, HomeComponent, LoginComponent, NavbarComponent, CoursesAssignedComponent]
 })
 export class AppComponent {
   title = 'gm-frontend';
